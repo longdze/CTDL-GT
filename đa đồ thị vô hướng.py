3a06306cc5df756864780673079570b5f6dbdc96
longdze <115168657+longdze@users.noreply.github.com>
2023-05-24 21:22:17 +0700

    Add files via upload

---
+++ đa đồ thị vô hướng.py
@@ -0,0 +1,25 @@
+class Do_thi:
+    def __init__(self):
+        self.canh = []
+
+    def them_canh(self, src, dest):
+        # Thêm cạnh vào danh sách cạnh
+        self.canh.append((src, dest))
+
+    def in_do_thi(self):
+        for canh in self.canh:
+            src, dest = canh
+            print("Cạnh:", src, "-", dest)
+
+# Tạo một đa đồ thị vô hướng
+do_thi = Do_thi()
+# Thêm các cạnh vào đồ thị
+do_thi.them_canh(0, 1)
+do_thi.them_canh(0, 1)  # Thêm cạnh lặp lại
+do_thi.them_canh(1, 2)
+do_thi.them_canh(2, 2)  # Thêm self-loop
+do_thi.them_canh(2, 3)
+do_thi.them_canh(3, 0)
+
+# In đồ thị
+do_thi.in_do_thi()
---
+++ đơn đồ thị có hướng.py
@@ -0,0 +1,29 @@
+class Do_thi:
+    def __init__(self, so_dinh):
+        self.so_dinh = so_dinh
+        self.adj_list = [[] for _ in range(so_dinh)]
+
+    def them_canh(self, src, dest):
+        # Thêm cạnh từ đỉnh nguồn đến đỉnh đích
+        self.adj_list[src].append(dest)
+
+    def in_do_thi(self):
+        for v in range(self.so_dinh):
+            print("Đỉnh", v, ":", end=" ")
+            for neighbor in self.adj_list[v]:
+                print(neighbor, end=" ")
+            print()
+
+# Tạo một đơn đồ thị có hướng với 5 đỉnh
+do_thi = Do_thi(5)
+# Thêm các cạnh vào đồ thị
+do_thi.them_canh(0, 1)
+do_thi.them_canh(0, 4)
+do_thi.them_canh(1, 2)
+do_thi.them_canh(1, 3)
+do_thi.them_canh(1, 4)
+do_thi.them_canh(2, 3)
+do_thi.them_canh(3, 4)
+
+# In đồ thị
+do_thi.in_do_thi()
